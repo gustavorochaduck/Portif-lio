f39eed5e34d5ebec500d82c5c9ea7b95ca5a073b
gustavorochaduck <gustavorochaduck@gmail.com>
2025-02-23 20:46:02 +0100

    Übungsaufgaben Python – KA 3

--- fcbg/informatic/python/KA vorbereitung/ex06.py
+++ fcbg/informatic/python/KA vorbereitung/ex06.py
@@ -1,6 +1,14 @@
 import random
 
 userAntwort = input('Wählen Sie eine Zahl bis 100: ')
-programAntwort = random.uniform(1,10)
+programAntwort = int(random.uniform(1,100))
 print('-=-'*20)
-print(programAntwort)
+
+if programAntwort == userAntwort:
+    print(f'\nRichtige Antwort!!!\nAntwort: {programAntwort}')
+
+if programAntwort != userAntwort:
+    print(f'Falsche Antwort!!!\nAntwort: {programAntwort}')
+
+else:
+    print('ERROR!!!')
---
+++ fcbg/informatic/python/KA vorbereitung/ex07.py
@@ -0,0 +1,8 @@
+n1 = int(input('Startnummer: '))
+n2 = int(input('Endzahl: '))
+
+summe = n1+1
+
+for x in range(n1, n2-1):
+    print(summe)
+    summe += 1
