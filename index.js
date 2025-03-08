7deb77072374d74c0ed7ac38c6e46cafddfea07f
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2025-03-08 23:16:14 +0100

    JS BETA Notification

--- index.js
+++ index.js
@@ -1,2 +1,1 @@
-var i = 0;
-var text_01 = "Hi, I like to make simiple websites like this: <a id="wheater-link-1" href="https://gustavorochaduck.github.io/wheather/">wheather</a><br>I am not a expert in Html, Javascrip and CSS, but i wan't to learn<br>I now a litle bit of this lenguege: Html, Javascrip, CSS and PythonAnd i am a <a id='Filmmaker-link-1' href="https://www.youtube.com/@gustavocineproducoes2686">Filmmaker</a> and in the bottom of this website, have all my projects</p><a class="donation_link" href="https://gustavorochaduck.github.io/Portif-lio/donate/donate.html"><button class="donation_buttom">Donation</button></a>";
+window.alert("This is a BETA");
